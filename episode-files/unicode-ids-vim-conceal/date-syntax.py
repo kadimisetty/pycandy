e58cef10b8d872d5bc353dbb8cc0a631216a44c4
Sri Kadimisetty <sri.kadimisetty@gmail.com>
2013-07-09 04:12:20 -0500

    add a sqrt

---
+++ episode-files/unicode-ids-vim-conceal/date-syntax.py
@@ -0,0 +1,3 @@
+from datetime import datetime
+
+print('The time now is - {}'.format(datetime.now()))
---
+++ episode-files/unicode-ids-vim-conceal/sqrt.py
@@ -0,0 +1,4 @@
+import math
+
+n = 16
+print('Square root of {} is {}'.format(n, math.sqrt(n)))
